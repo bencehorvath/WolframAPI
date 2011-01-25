19511bff2d5c7a5856abb5dfabd30d7c50b57b03
Twl <twl@null.net>
2011-01-25 17:07:33 +0100

    * Add the GetResult method with which users can manually handle the parsed response in any way they'd like. This allows them to retrieve images, manually iterate through pods, etc.
    * Handle exceptions which might be thrown in the current code and throw a WolframException when one occurs. The InnerException property is set to the original exception.

--- WolframAPI/WAClient.cs
+++ WolframAPI/WAClient.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using WolframAPI.Exceptions;
+
 namespace WolframAPI
 {
     using System;
@@ -54,25 +56,54 @@ namespace WolframAPI
         }
 
         /// <summary>
+        /// Gets the result of the specified expression.
+        /// <para>The expression is returned as <see cref="WAResult"/>WAResult</para> 
+        /// so you can manually go through the pods of the response (to get ANY information you'd like)
+        /// <para>It is encouraged to use this method instead of <see cref="Solve"/>Solve</para>
+        /// </summary>
+        /// <param name="expression">The expression to solve.</param>
+        /// <exception cref="WolframException">Throws in case of any error.</exception>
+        /// <returns>The result</returns>
+        public WAResult GetResult(string expression)
+        {
+            var response = Submit(expression);
+            var result = Parse(response);
+
+            return result;
+        }
+
+        /// <summary>
         /// Submits the specified expression and returns the raw result.
         /// </summary>
         /// <param name="expression">The expression to post.</param>
+        /// <exception cref="WolframException">Throws in case of any error.</exception>
         /// <returns>Raw response</returns>
         public string Submit(string expression)
         {
-            expression = expression.Replace("=", " = ");
-            var wacode = HttpUtility.UrlEncode(expression);
-
-            var url = new Uri(string.Format(BaseUrl, _appId, wacode));
-
-            string returned;
-
-            using(var client = new WebClient())
+            try
             {
-                returned = client.DownloadString(url);
-            }
+                expression = expression.Replace("=", " = ");
+                var wacode = HttpUtility.UrlEncode(expression);
 
-            return returned;
+                var url = new Uri(string.Format(BaseUrl, _appId, wacode));
+
+                string returned;
+
+                using (var client = new WebClient())
+                {
+                    returned = client.DownloadString(url);
+                }
+
+                return returned;
+            }
+            catch(WebException x)
+            {
+                throw new WolframException("WebException thrown while getting the response.", x);
+            }
+            catch(Exception x)
+            {
+                throw new WolframException("Unhandled exception thrown while submitting the expression.", x);
+            }
         }
 
         /// <summary>
@@ -80,18 +111,30 @@ namespace WolframAPI
         /// </summary>
         /// <param name="response">The response to parse</param>
         /// <returns>The parsed response</returns>
+        /// <exception cref="WolframException">Throws in case of any error.</exception>
         public WAResult Parse(string response)
         {
-            var serializer = new XmlSerializer(typeof (WAResult));
-
-            WAResult result;
-
-            using(var reader = new StringReader(response))
+            try
             {
-                result = serializer.Deserialize(reader) as WAResult;
-            }
+                var serializer = new XmlSerializer(typeof (WAResult));
 
-            return result;
+                WAResult result;
+
+                using (var reader = new StringReader(response))
+                {
+                    result = serializer.Deserialize(reader) as WAResult;
+                }
+
+                return result;
+            }
+            catch(ArgumentNullException x)
+            {
+                throw new WolframException("Argument null exception thrown. The response passed to Parse() was probably empty.", x);
+            }
+            catch(InvalidOperationException x)
+            {
+                throw new WolframException("Exception thrown while deserializing the response.", x);
+            }
         }
     }
 }
